cc914033dc448a1ef0ea4463cbac65e6dac1bbd6
Raphael Bravim <raphael.bravim@gmail.com>
2021-09-23 15:08:39 -0300

    Update raw-test.py

--- raw-test.py
+++ raw-test.py
@@ -1,7 +1,7 @@
 import mysql.connector
 import http.client, mimetypes, json
 import random
-from datetime import datetime, timedelta #comentario para teste 2
+from datetime import datetime, timedelta
 
 name = ['Julio', 'Julia', 'Gabriele', 'Gabriel', 'Jonathan', 'Natan', 'Wagner', 'Adalgiza', 'João', 'Lucas', \
         'Mayara', 'Mariana', 'Aline', 'Alice', 'Renata', 'Marisbela', 'Rafael', 'Claudia', 'Vitória', 'Jorge', \
